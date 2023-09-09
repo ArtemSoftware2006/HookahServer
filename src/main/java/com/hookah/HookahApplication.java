0945c70bd3c7c385592ec4c7ee2f916c8571d96c
Artem <artem.egorov.art2006@gmail.com>
2023-09-09 21:04:59 +0300

    init and test

--- src/main/java/com/hookah/HookahApplication.java
+++ src/main/java/com/hookah/HookahApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HookahApplication {
 
 	public static void main(String[] args) {
+		
+		System.out.print("rtereterterterteter");
 		SpringApplication.run(HookahApplication.class, args);
 	}
 
